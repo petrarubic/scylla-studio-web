b5f1d2c66b9a760b52f9e200e9eabb5cdf70016b
Petra Rubic <prubic.dev@gmail.com>
2021-07-19 12:18:13 +0200

    Add scroll to top as default when loading new page

--- src/app/ui/main.cljs
+++ src/app/ui/main.cljs
@@ -3,12 +3,16 @@
             [keechma.next.helix.lib :refer [defnc]]
             [helix.core :as hx :refer [$]]
             [helix.dom :as d]
+            [helix.hooks :as hooks]
+            [oops.core :refer [ocall]]
             [app.ui.pages.home :refer [Home]]
             [app.ui.pages.news :refer [News]]
             [app.ui.pages.team :refer [Team]]))
 
 (defnc MainRenderer [props]
-  (let [{:keys [page]} (use-sub props :router)]
+  (let [{:keys [page] :as router} (use-sub props :router)]
+    (hooks/use-effect [(pr-str router)]
+                      (ocall js/window :scrollTo 0 0))
     (case page
       "home" ($ Home)
       "news" ($ News)
